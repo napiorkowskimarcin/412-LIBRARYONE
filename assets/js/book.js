7c41b0c596bff3b6c0a2003bd3ffb698b81e385c
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2021-02-15 21:39:37 +0100

    book - ajax refactor

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,52 +1,38 @@
 const path = "/book/show";
+let data = "";
+
+let sendAjax = {
+  ajax: true,
+  type: "get",
+  url: path,
+  // data: "",
+  dataType: "html",
+  success: function (data) {
+    response = JSON.parse(data);
+    $("#table").html(response.template);
+  },
+};
 
 $("#title").keyup(function (event) {
   event.preventDefault();
+  sendAjax.data = $("#inputTitle");
   if ($("#inputTitle").val().length) {
-    $.ajax({
-      ajax: true,
-      type: "get",
-      url: path,
-      data: $("#inputTitle"),
-      dataType: "html", // for json response or 'html' for html response
-      success: function (data) {
-        response = JSON.parse(data);
-        $("#table").html(response.template);
-      },
-    });
+    $.ajax(sendAjax);
   }
 });
 
 $("#author").keyup(function (event) {
   event.preventDefault();
+  sendAjax.data = $("#inputAuthor");
   if ($("#inputAuthor").val().length) {
-    $.ajax({
-      ajax: true,
-      type: "get",
-      url: path,
-      data: $("#inputAuthor"),
-      dataType: "html",
-      success: function (data) {
-        response = JSON.parse(data);
-        $("#table").html(response.template);
-      },
-    });
+    $.ajax(sendAjax);
   }
 });
 
 $("#isbn").keyup(function (event) {
   event.preventDefault();
+  sendAjax.data = $("#inputIsbn");
   if ($("#inputIsbn").val().length) {
-    $.ajax({
-      ajax: true,
-      type: "get",
-      url: path,
-      data: $("#inputIsbn"),
-      dataType: "html",
-      success: function (data) {
-        response = JSON.parse(data);
-        $("#table").html(response.template);
-      },
-    });
+    $.ajax(sendAjax);
   }
 });
